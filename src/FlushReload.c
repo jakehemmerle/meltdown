f6e1ec9b1b745032a6df584ed493831a541c492a
Jake Hemmerle <jakehemmerle@protonmail.com>
2020-05-01 12:42:00 -0400

    latest

--- src/FlushReload.c
+++ src/FlushReload.c
@@ -13,4 +13,33 @@ char secret = 94;
 void flushSideChannel() {
   int i;
   for (in = 0; i < 256; i++) array[i*4096 + DELTA] = 1;
+  for (i = 0; i < 256; i++) __mm_clflush(&array[i*4096 + DELTA]);
+}
+
+void victim() {
+  temp = array[secret*4096 + DELTA];
+}
+
+void reloadSideChannel() {
+  int junk = 0;
+  register uint64_t time1, time2;
+  volatile uint8_t *addr;
+  int i;
+  for(i = 0, i < 256; i++) {
+    addr = &array[i*4096 + DELTA];
+    time1 = __rdtscp(&junk);
+    junk = *addr;
+    time2 = __rdtscp(&junk) - time1;
+    if (time2 <= CACHE_HIT_THRESHOLD) {
+      printf("array[%d*4096 + %d] is in cache.\n", i, DELTA);
+      printf("The Secret = %d.\n", i);
+    }
+  }
+}
+
+int main(int argc, const char **argv) {
+  flushSideChannel();
+  victim();
+  reloadSideChannel();
+  return 0;
 }
